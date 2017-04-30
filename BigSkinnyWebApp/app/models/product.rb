07a9bca51a027e5a59e21df506b74860990ec43b
Alec Lam <ahlam@andrew.cmu.edu>
2017-04-30 19:42:49 -0400

    writing models of Products, bug with inventory/price validations

--- BigSkinnyWebApp/app/models/product.rb
+++ BigSkinnyWebApp/app/models/product.rb
@@ -1,2 +1,20 @@
 class Product < ApplicationRecord
+
+  # List of allowable categories
+  CATEGORIES = ['Men\'s', 'Women\'s', 'Kid\'s']
+
+  #scopes
+  scope :cart,   -> {where(cart: true)}
+  scope :need_reorder, -> {where('inventory < 20')}
+  scope :alphabetical, -> {order(:name)}
+  scope :mens, -> {where("categories = 'Men\'s' ")}
+  scope :womens, -> {where("categories = 'Women\'s' ")}
+  scope :kids, -> {where("categories = 'Kid\'s' ")}
+
+  #validations
+  validates_presence_of :name, :weight, :description, :dimensions, :color, :category, :inventory, :price
+  validates_inclusion_of :category, in: CATEGORIES, message: "is not an option"
+  validates_numericality_of :inventory, only_integer: true, greater_than_or_equal_to: 0
+  validates_numericality_of :price, greater_than: 0
+
 end
