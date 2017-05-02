16345039083c193490c7c917968f56dd50ad6e84
Alec Lam <ahlam@andrew.cmu.edu>
2017-05-02 01:04:23 -0400

    Finalized cart and checkout

--- BigSkinnyWebApp/app/controllers/home_controller.rb
+++ BigSkinnyWebApp/app/controllers/home_controller.rb
@@ -16,9 +16,19 @@ class HomeController < ApplicationController
 
   def cart
     @ordered_items = Product.all.cart
+
   end
 
   def download_pdf
     send_file 'app/assets/images/schedule.pdf'
+  end
+
+  def checkout
+    cart_items = Product.all.cart
+    for item in cart_items do
+      item.cart = false
+      item.save!
+    end
+    redirect_to cart_path, notice: "Checked out! Thank you for Shopping with Big Skinny!"
   end
 end
